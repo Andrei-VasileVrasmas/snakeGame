0dcc7a9422319b95909f07d0e3769afa81aabbd5
Andrei Vasile <vrasmas_vasy@yahoo.com>
2019-07-05 19:42:50 +0100

    New structure of the code in more files

---
+++ gameControl.js
@@ -0,0 +1,56 @@
+const updateGameArea = () => {
+  myGameArea.clear();
+  playerObject.update();
+}
+
+const moveUp = () => {
+  playerObject.y -= 20;
+  if (playerObject.y <= 0) {
+    playerObject.y = 370;
+
+  }
+}
+
+const moveDown = () => {
+  playerObject.y += 20;
+  if (playerObject.y >= 390) {
+    playerObject.y = 10;
+  }
+}
+
+const moveLeft = () => {
+  playerObject.x -= 20;
+  if (playerObject.x <= 0) {
+    playerObject.x = 370;
+
+  }
+}
+
+const moveRight = () => {
+  playerObject.x += 20;
+  if (playerObject.x >= 390) {
+    playerObject.x = 10;
+  }
+}
+
+const checkKey = (e) => {
+  e = e || window.event;
+
+  if (e.keyCode === 37) {
+    moveLeft();
+  }
+
+  if (e.keyCode === 39) {
+    moveRight();
+  }
+
+  if (e.keyCode === 38) {
+    moveUp();
+  }
+
+  if (e.keyCode === 40) {
+    moveDown();
+  }
+}
+
+document.onkeydown = checkKey;
---
+++ gameSetup.js
@@ -0,0 +1,22 @@
+let playerObject;
+
+
+const myGameArea = {
+  canvas: document.createElement("canvas"),
+  start() {
+    this.canvas.width = 400;
+    this.canvas.height = 400;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.interval = setInterval(updateGameArea, 20);
+  },
+  clear() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+}
+
+
+const startGame = () => {
+  myGameArea.start();
+  playerObject = new Player(20, 20, "red", 190, 190, myGameArea.context);
+}
