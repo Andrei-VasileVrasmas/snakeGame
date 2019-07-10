bad61a8746961bc628115caa27101e35dd37fc22
Andrei Vasile <vrasmas_vasy@yahoo.com>
2019-07-10 19:31:06 +0100

    added first block/ not working porperly

--- classes/playerClass.js
+++ classes/playerClass.js
@@ -7,15 +7,26 @@ class Player {
     this.color = color;
     this.ctx = ctx;
     this.score = 0;
+    this.blocks = [];
+    this.lastX = x;
+    this.lastY = y;
   }
 
   update() {
     this.ctx.fillStyle = this.color;
     this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    for (let i = 0; i < this.blocks.length; i += 1) {
+      this.ctx.fillRect(this.blocks[i].x, this.blocks[i].y, this.width, this.height);
+    }
   }
 
   addScore() {
     this.score += 1;
     document.getElementById('score').innerHTML = 'Score:' + this.score;
+
+    this.blocks.push({
+      x: this.lastX,
+      y: this.lastY,
+    })
   }
 }
--- gameControl.js
+++ gameControl.js
@@ -9,8 +9,15 @@ const moveUp = () => {
   playerObject.y -= 20;
   if (playerObject.y < 0) {
     playerObject.y = 380;
-
   }
+  /*
+  for (let i = 0; i < playerObject.blocks.length; i += 1) {
+    playerObject.blocks[i].y -= 20;
+    if (playerObject.blocks[i].y < 0) {
+      playerObject.blocks[i].y = 380;
+    }
+  }
+  */
 }
 
 const moveDown = () => {
@@ -18,13 +25,26 @@ const moveDown = () => {
   if (playerObject.y >= 400) {
     playerObject.y = 0;
   }
+
+  for (let i = 0; i < playerObject.blocks.length; i += 1) {
+    playerObject.blocks[i].y += 20;
+    if (playerObject.blocks[i].y < 0) {
+      playerObject.blocks[i].y = 380;
+    }
+  }
 }
 
 const moveLeft = () => {
   playerObject.x -= 20;
   if (playerObject.x < 0) {
     playerObject.x = 380;
+  }
 
+  for (let i = 0; i < playerObject.blocks.length; i += 1) {
+    playerObject.blocks[i].x -= 20;
+    if (playerObject.blocks[i].x < 0) {
+      playerObject.blocks[i].x = 380;
+    }
   }
 }
 
@@ -33,9 +53,18 @@ const moveRight = () => {
   if (playerObject.x >= 400) {
     playerObject.x = 0;
   }
+  for (let i = 0; i < playerObject.blocks.length; i += 1) {
+    playerObject.blocks[i].x += 20;
+    if (playerObject.blocks[i].x < 0) {
+      playerObject.blocks[i].x = 380;
+    }
+  }
 }
 
 const checkKey = (e) => {
+  playerObject.lastY = playerObject.y;
+  playerObject.lastX = playerObject.x;
+
   e = e || window.event;
 
   if (e.keyCode === 37) {
