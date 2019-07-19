a6a25550fcf6a6c582e735bcab7cee041c33b05f
Andrei Vasile <vrasmas_vasy@yahoo.com>
2019-07-19 17:59:42 +0100

    Snake respawn at the other end of the canvas & check is it eats the tail

--- classes/playerClass.js
+++ classes/playerClass.js
@@ -12,7 +12,6 @@ class Player {
     this.lastTailMove = null;
   }
 
-
   update() {
     this.ctx.fillStyle = this.color;
     this.ctx.fillRect(this.x, this.y, this.width, this.height);
@@ -24,22 +23,38 @@ class Player {
       for (let b = 0; b <= i; b += 1) {
         if (this.blocks[b] === 'up') {
           posY += 20;
+          if (posY >= 400) {
+            posY = 0;
+          }
         }
         if (this.blocks[b] === 'down') {
           posY -= 20;
+          if (posY < 0) {
+            posY = 380;
+          }
         }
         if (this.blocks[b] === 'left') {
           posX += 20;
+          if (posX >= 400) {
+            posX = 0;
+          }
         }
         if (this.blocks[b] === 'right') {
           posX -= 20;
+          if (posX < 0) {
+            posX = 380;
+
+          }
         }
       }
 
       this.ctx.fillRect(posX, posY, this.width, this.height);
+
+      if (this.x === posX && this.y === posY) {
+        console.log('Failed'); // this.x is the head the first block, same as this.y while posY and posX are the other blocks of the array
+      }
     }
   }
-
 
   addScore() {
     this.score += 1;
--- gameSetup.js
+++ gameSetup.js
@@ -8,8 +8,9 @@ const myGameArea = {
     this.canvas.height = 400;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.interval = setInterval(updateGameArea, 20);
-  },
+   this.interval = setInterval(updateGameArea, 20);
+   
+ },
   clear() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
